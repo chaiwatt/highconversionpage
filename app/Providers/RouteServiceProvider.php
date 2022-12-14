82eda6526922f7e715a86d1ca485f0a1011273b8
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-14 17:15:27 +0700

    update some

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,7 +33,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::domain('{subdomain}.highconversionpage.local')
+            Route::domain('{subdomain}.highconversionpage.com')
                 ->group(base_path('routes/subdomain_web.php'));       
 
             Route::middleware('web')
