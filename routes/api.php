ead02b83a569c94ba4833d31b9765b08debee858
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-20 13:06:46 +0700

    update some

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebHookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('webhook', [WebHookController::class, 'webhook'])->name('webhook');
+Route::post('webhook_demo', [WebHookController::class, 'webhook_demo'])->name('webhook_demo');
